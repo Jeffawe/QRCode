667fcaeb01edb1e8ba151593e5f5be4e0e9c2131
Jeffawe <awagujeffery@gmail.com>
2022-12-27 12:01:47 +0100

    Added a Favicon and Built the Project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,14 @@ import './QR State/qrstate.css'
 
 const Footer = () => {
     return (
-        <div className='footer'><a href='https://jeffery-oa.vercel.app/'>Created by Jeffery Ozoekwe-Awagu</a></div>
+        <div className='footer'>
+            <div>
+                <a href='https://jeffery-oa.vercel.app/'>Created by Jeffery Ozoekwe-Awagu</a>
+            </div>
+            <div>
+                <a href="https://icons8.com/icon/xlIKHY5rHY3r/qr-code-scan">Qr Code Scan icon by Icons8</a>
+            </div>
+        </div>
     )
 }
 
