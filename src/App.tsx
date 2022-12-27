687e89a5ccce41a163ee6dfcfd49f53ddcbe5947
Jeffawe <awagujeffery@gmail.com>
2022-12-27 11:26:41 +0100

    Added a Footer and Fixed Responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import EmailWork from './components/Page/input'
 import NavWork from './components/Navbar/navbar'
+import Footer from './components/Footer';
 import './App.css';
 
 const App: React.FC = () => {
@@ -11,6 +12,9 @@ const App: React.FC = () => {
       </div>
       <div>
         <EmailWork />
+      </div>
+      <div>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './QR State/qrstate.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'><a href='https://jeffery-oa.vercel.app/'>Created by Jeffery Ozoekwe-Awagu</a></div>
+    )
+}
+
+export default Footer
